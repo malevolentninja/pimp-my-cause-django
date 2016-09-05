b62e6c71f76b04dfd21870c76553c86dfabb959d
Liliana Kastilio <kastilio.liliana@gmail.com>
2016-09-05 22:05:01 +0100

    Correcting a typo

--- pimpmycause/pimpmycause/wsgi.py
+++ pimpmycause/pimpmycause/wsgi.py
@@ -12,7 +12,7 @@ from whitenoise.django import DjangoWhiteNoise
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "fashrevwall.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "pimpmycause.settings")
 
 application = get_wsgi_application()
 application = DjangoWhiteNoise(application)
